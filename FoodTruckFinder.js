22d465704198ccec47c8d9d1f1a8045db231977e
Alex Wander <alexcwander@gmail.com>
2021-03-18 20:29:34 -0500

    Created fixTime function

--- FoodTruckFinder.js
+++ FoodTruckFinder.js
@@ -12,72 +12,83 @@ getTime = () => {
 	};
 };
 
+// if value is less than or equal to 9 it comes back without the 0
+fixTime = (n) => {
+	return (n = '0' + n);
+};
+
 getFoodTrucks = (offset) => {
 	// Refresh time for each request
 	getTime();
 
 	// fix hour and minutes
-	// if value is less than or equal to 9 it comes back without the 0
-	if (sysTime.hour < 9) {
-		sysTime.hour = '0' + sysTime.hour;
+	if (sysTime.hour <= 9) {
+		sysTime.hour = fixTime(sysTime.hour);
 	}
 
-	if (sysTime.minutes < 9) {
-		sysTime.minutes = '0' + sysTime.minutes;
+	if (sysTime.minutes <= 9) {
+		sysTime.minutes = fixTime(sysTime.minutes);
 	}
 
 	var currentTime = sysTime.hour + `:` + sysTime.minutes;
 
-  // API options
-  var baseURL = `http://data.sfgov.org/resource/bbb8-hzi6.json?`;
-  var dayOrder = `&dayorder=${sysTime.day}`; // set the current day
-  var openTime = `&$where=start24<='${currentTime}'%20and%20end24>'${currentTime}'`; // set the time range
-  var dataSelect = `&$select=applicant,location,start24,end24`; // set what data to return for each truck
-  var sort = `&$order=applicant`; // sort in ascending order by name
-  var page = `&$limit=${limit}&$offset=${offset}`; // set the max # of trucks to return and offset for pages
+	// API options
+	var baseURL = `http://data.sfgov.org/resource/bbb8-hzi6.json?`;
+	var dayOrder = `&dayorder=${sysTime.day}`; // set the current day
+	var openTime = `&$where=start24<='${currentTime}'%20and%20end24>'${currentTime}'`; // set the time range
+	var dataSelect = `&$select=applicant,location,start24,end24`; // set what data to return for each truck
+	var sort = `&$order=applicant`; // sort in ascending order by name
+	var page = `&$limit=${limit}&$offset=${offset}`; // set the max # of trucks to return and offset for pages
 
 	// Get food trucks request
-	request.get(`${baseURL}${dayOrder}${openTime}${dataSelect}${sort}${page}`,
-    function (error, response, body) {
-      // Print the error if one occurred
-      if (error != null) {
-        console.error('error:', error);
-      }
+	request.get(
+		`${baseURL}${dayOrder}${openTime}${dataSelect}${sort}${page}`,
+		function (error, response, body) {
+			// Print the error if one occurred
+			if (error != null) {
+				console.error('error:', error);
+			}
 
-      // Print the response status code if 200 not received
-      if (response.statusCode != 200) {
-        console.log('statusCode:', response && response.statusCode);
-      }
+			// Print the response status code if 200 not received
+			if (response.statusCode != 200) {
+				console.log('statusCode:', response && response.statusCode);
+			}
 
-      // Print the data if no error and status code success
-      if (!error && response.statusCode == 200) {
-      // Parse data from a string to a JS object
-      var openFoodTrucks = JSON.parse(body);
-      var numTrucks = openFoodTrucks.length;
+			// Print the data if no error and status code success
+			if (!error && response.statusCode == 200) {
+				// Parse data from a string to a JS object
+				var openFoodTrucks = JSON.parse(body);
+				var numTrucks = openFoodTrucks.length;
 
-      console.log(`Your local time is ${currentTime}.`);
+				console.log(`Your local time is ${currentTime}.`);
 
-      // Display trucks on page
-      for (var i = 0; i < numTrucks; i++) {
-        var truck = openFoodTrucks[i];
-        console.log(`${i + 1}. ${truck.applicant} is currently open. They are located at ${truck.location}. Their hours are ${truck.start24} to ${truck.end24}.\n`);
-      }
-      printMoreTrucks(numTrucks, offset);
-      }
-    }
-  )
+				// Display trucks on page
+				for (var i = 0; i < numTrucks; i++) {
+					var truck = openFoodTrucks[i];
+					console.log(
+						`${i + 1 + offset}. ${
+							truck.applicant
+						} is currently open. They are located at ${
+							truck.location
+						}. Their hours are ${truck.start24} to ${truck.end24}.\n`
+					);
+				}
+				printMoreTrucks(numTrucks, offset);
+			}
+		}
+	);
 };
 
 printMoreTrucks = (numTrucks, offset) => {
 	// If the previous return had max trucks per page, display more
 	if (numTrucks == limit) {
-    // Increment the offset by the page limit
-    offset += limit;
+		// Increment the offset by the page limit
+		offset += limit;
 
-    // Prompt the user to proceed
+		// Prompt the user to proceed
 		console.log('Press enter to see more open food trucks...');
 
-    // Wait for user to press key
+		// Wait for user to press key
 		stdin.addListener('data', function (data) {
 			stdin.removeAllListeners('data');
 			getFoodTrucks(offset);
